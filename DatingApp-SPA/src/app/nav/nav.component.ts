f3901439b3a994ff9347ed2f12232d80c6d63a83
unknown <jony.alv.dev@outlook.com>
2019-05-27 21:50:01 -0300

    Login desde el front OK

---
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    // console.log(this.model);
+    this.authService.login(this.model).subscribe(next => {
+      console.log('Logueado OK');
+    }, error => {
+      console.log('Error en el logueo');
+    });
+  }
+
+}
